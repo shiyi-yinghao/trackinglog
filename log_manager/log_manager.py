20cd575557a6e05e96e49b22e158ddd14e9239f4
Yinghao Li <shiyi.yinghao@gmail.com>
2024-08-10 14:03:26 -0400

    Dev v0.1.1

--- log_manager/log_manager.py
+++ log_manager/log_manager.py
@@ -1,21 +1,31 @@
 import logging
 import os
+from functools import wraps
+from .parameter_config import LogConfig
 
 class LogManager:
     _instance = None
-
+    
     def __new__(cls):
         if not cls._instance:
             cls._instance = super(LogManager, cls).__new__(cls)
-            cls._instance.config = {}
-            cls._instance.logger_dict = {}
+            cls._instance.config = LogConfig()
         return cls._instance
 
-    def setup(self, **kwargs):
-        self.config = kwargs
-        self.root_log_path = self.config.get('root_log_path', 'logs')
-        if not os.path.exists(self.root_log_path):
-            os.makedirs(self.root_log_path)
+    def setup(self, root_log_path=None):
+        assert self.check_root_log_path(root_log_path), f"Invalid Root Log Path Given: {root_log_path}"
+        self.config.root_log_path = root_log_path
+        
+
+    def check_root_log_path(self, root_log_path):
+        if not root_log_path:
+            return False
+        try:
+            os.makedirs(self.config.root_log_path, exist_ok=True)
+        except Exception as e:
+            print("Error during creating the folder.",e)
+            return False
+        return True
 
     def setup_check(self):
         if not hasattr(self, 'root_log_path') or not self.root_log_path:
@@ -39,19 +49,23 @@ class LogManager:
         self.logger_dict[logname] = logger
         return logger
 
-    def get_log(self, logname, filename=None, folderpath=None, log_level=logging.DEBUG):
+    def get_log(self, logname, filename=None, folderpath=None, log_level=logging.DEBUG, func=None):
         if logname not in self.logger_dict:
             self.create_logger(logname, filename, folderpath, log_level)
-
-        def decorator(func):
-            logger = self.logger_dict[logname]
-            def wrapper(*args, **kwargs):
-                logger.debug(f'Starting function {func.__name__}')
-                result = func(*args, **kwargs)
-                logger.debug(f'Function {func.__name__} ended.')
-                return result
-            return wrapper
-        return decorator
+        
+        logger = self.logger_dict[logname]
+        
+        if func is None:
+            return logger
+        
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            kwargs['log'] = logger  # Inject the logger into the function's keyword arguments
+            logger.debug(f'Starting function {func.__name__}')
+            result = func(*args, **kwargs)
+            logger.debug(f'Function {func.__name__} ended.')
+            return result
+        return wrapper
 
     def get_logger(self, logname):
         self.setup_check()
